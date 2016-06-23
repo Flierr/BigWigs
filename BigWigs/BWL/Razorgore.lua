8f0f54906596e203f4e4c37dabcebef2a05a0ee6
isitLoVe <isitl0v3@gmail.com>
2016-06-23 20:16:52 +0200

    rev 19001

--- BigWigs/BWL/Razorgore.lua
+++ BigWigs/BWL/Razorgore.lua
@@ -52,7 +52,7 @@ BigWigsRazorgore = BigWigs:NewModule(boss)
 BigWigsRazorgore.zonename = AceLibrary("Babble-Zone-2.2")["Blackwing Lair"]
 BigWigsRazorgore.enabletrigger = { boss, controller }
 BigWigsRazorgore.toggleoptions = { "mc", "eggs", "phase", "bosskill" }
-BigWigsRazorgore.revision = tonumber(string.sub("$Revision: 17555 $", 12, -3))
+BigWigsRazorgore.revision = tonumber(string.sub("$Revision: 19001 $", 12, -3))
 
 ------------------------------
 --      Initialization      --
