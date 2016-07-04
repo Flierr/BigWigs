4ac2e721c606d8e513464babc6443424004149fc
Flierr <flierrgaming@gmail.com>
2016-07-05 00:37:57 +0200

    Update Firemaw.lua

--- BigWigs/BWL/Firemaw.lua
+++ BigWigs/BWL/Firemaw.lua
@@ -14,7 +14,7 @@ L:RegisterTranslations("enUS", function() return {
 	shadowflame_trigger = "Firemaw begins to cast Shadow Flame.",
     flame_trigger = "afflicted by Flame Buffet",
 
-	startwarn = "Firemaw Engaged! First Wing Buffet in 26 seconds!",
+	startwarn = "Firemaw Engaged! First Wing Buffet in 27 seconds!",
 	wingbuffet_message = "Wing Buffet! 25sec to next!",
 	wingbuffet_warning = "3sec to Wing Buffet!",
 	shadowflame_warning = "Shadow Flame Incoming!",
@@ -94,8 +94,8 @@ function BigWigsFiremaw:BigWigs_RecvSync( sync, rest )
 		self:TriggerEvent("BigWigs_SendSync", "FiremawStart")
 	elseif sync == "FiremawStart" and self.db.profile.wingbuffet then
 		self:TriggerEvent("BigWigs_Message", L["startwarn"], "Important")
-		self:ScheduleEvent("BigWigs_Message", 22, L["wingbuffet_warning"], "Important", true, "Alarm")
-		self:TriggerEvent("BigWigs_StartBar", self, L["wingbuffet_bar"], 25, "Interface\\Icons\\Spell_Fire_SelfDestruct")
+		self:ScheduleEvent("BigWigs_Message", 24, L["wingbuffet_warning"], "Important", true, "Alarm")
+		self:TriggerEvent("BigWigs_StartBar", self, L["wingbuffet_bar"], 27, "Interface\\Icons\\Spell_Fire_SelfDestruct")
 	elseif sync == "FiremawWingBuffet" and self.db.profile.wingbuffet then
 		self:TriggerEvent("BigWigs_Message", L["wingbuffet_message"], "Important")
 		self:ScheduleEvent("BigWigs_Message", 22, L["wingbuffet_warning"], "Important", true, "Alarm")
