5587d50afe495bc1b303fa5334439d3b819fd33d
isitLoVe <isitl0v3@gmail.com>
2016-07-12 21:53:51 +0200

    Update Anubrekhan.lua
    locust swarm initial fix
    added impale timer
    first locust is dynamic 100sec - 115sec , the following locusts are 90sec 
    impale is cast every 15 sec and 45 sec after the last impale during locust
    
    https://github.com/Flierr/BigWigs/issues/26

--- Interface/Addons/BigWigs/Naxxramas/Anubrekhan.lua
+++ Interface/Addons/BigWigs/Naxxramas/Anubrekhan.lua
@@ -1,4 +1,4 @@
-﻿------------------------------
+------------------------------
 --      Are you local?      --
 ------------------------------
 
@@ -19,19 +19,27 @@ L:RegisterTranslations("enUS", function() return {
 	buff_cmd = "buff",
 	buff_name = "Buff Alert",
 	buff_desc = "Notify on Buffs Fading",
+
+	impale_cmd = "impale",
+	impale_name = "Impale Timer",
+	impale_desc = "Timer for Impale",
 	
 	starttrigger1 = "Just a little taste...",
-	starttrigger2 = "Yes, Run! It makes the blood pump faster!",
+	starttrigger2 = "Yes, run! It makes the blood pump faster!",
 	starttrigger3 = "There is no way out.",
 	engagewarn = "Anub'Rekhan engaged. First Locust Swarm in ~90 sec",
 	GNPPtrigger	= "Nature Protection",
 
 	gaintrigger = "Anub'Rekhan gains Locust Swarm.",
 	gainendwarn = "Locust Swarm ended!",
-	gainnextwarn = "Next Locust Swarm in ~85 sec",
-	gainwarn10sec = "~10 Seconds until Locust Swarm",
+	gainnextwarn = "Next Locust Swarm in 90 sec",
+	gainwarn10sec = "10 Seconds until Locust Swarm",
 	gainincbar = "Next Locust Swarm",
 	gainbar = "Locust Swarm",
+	
+	impaletrigger = "Impale",
+	impalebar = "Impale",
+	impalebarlocust = "Impale after Locust",
 
 	casttrigger = "Anub'Rekhan begins to cast Locust Swarm.",
 	castwarn = "Incoming Locust Swarm!",
@@ -45,8 +53,8 @@ L:RegisterTranslations("enUS", function() return {
 BigWigsAnubrekhan = BigWigs:NewModule(boss)
 BigWigsAnubrekhan.zonename = AceLibrary("Babble-Zone-2.2")["Naxxramas"]
 BigWigsAnubrekhan.enabletrigger = boss
-BigWigsAnubrekhan.toggleoptions = {"locust", "buff", "bosskill"}
-BigWigsAnubrekhan.revision = tonumber(string.sub("$Revision: 19001 $", 12, -3))
+BigWigsAnubrekhan.toggleoptions = {"locust", "impale", "buff", "bosskill"}
+BigWigsAnubrekhan.revision = tonumber(string.sub("$Revision: 19007 $", 12, -3))
 
 ------------------------------
 --      Initialization      --
@@ -60,10 +68,16 @@ function BigWigsAnubrekhan:OnEnable()
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_CREATURE_BUFFS")
 	self:RegisterEvent("CHAT_MSG_SPELL_AURA_GONE_SELF")
 	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS")
+	
+	--Impale
+	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_SELF_DAMAGE", "ImpaleCast")
+	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_PARTY_DAMAGE", "ImpaleCast")
+	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE", "ImpaleCast")
 
 	self:RegisterEvent("BigWigs_RecvSync")
 	self:TriggerEvent("BigWigs_ThrottleSync", "AnubLocustInc", 10)
 	self:TriggerEvent("BigWigs_ThrottleSync", "AnubLocustSwarm", 10)
+	self:TriggerEvent("BigWigs_ThrottleSync", "AnubImpale", 10)
 end
 
 function BigWigsAnubrekhan:CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS( msg )
@@ -85,8 +99,18 @@ end
 function BigWigsAnubrekhan:CHAT_MSG_MONSTER_YELL( msg )
 	if self.db.profile.locust and msg == L["starttrigger1"] or msg == L["starttrigger2"] or msg == L["starttrigger3"] then
 		self:TriggerEvent("BigWigs_Message", L["engagewarn"], "Urgent")
-		self:ScheduleEvent("BigWigs_Message", 80, L["gainwarn10sec"], "Urgent")
-		self:TriggerEvent("BigWigs_StartBar", self, L["gainincbar"], 90, "Interface\\Icons\\Spell_Nature_InsectSwarm")
+		self:ScheduleEvent("BigWigs_Message", 90, L["gainwarn10sec"], "Urgent")
+		self:TriggerEvent("BigWigs_StartBar", self, L["gainincbar"], 100, "Interface\\Icons\\Spell_Nature_InsectSwarm")
+	end
+	if self.db.profile.impale and msg == L["starttrigger1"] or msg == L["starttrigger2"] or msg == L["starttrigger3"] then
+		self:TriggerEvent("BigWigs_StartBar", self, L["impalebar"], 15, "Interface\\Icons\\INV_Weapon_ShortBlade_25")
+		--impale ist casted every 15 sec and 45 sec after the last locust+impale
+		self:ScheduleEvent("BigWigs_StartBar", 120, self, L["impalebarlocust"], 15, "Interface\\Icons\\INV_Weapon_ShortBlade_25")
+		self:ScheduleEvent("BigWigs_StartBar", 210, self, L["impalebarlocust"], 15, "Interface\\Icons\\INV_Weapon_ShortBlade_25")
+		self:ScheduleEvent("BigWigs_StartBar", 300, self, L["impalebarlocust"], 15, "Interface\\Icons\\INV_Weapon_ShortBlade_25")
+		self:ScheduleEvent("BigWigs_StartBar", 390, self, L["impalebarlocust"], 15, "Interface\\Icons\\INV_Weapon_ShortBlade_25")
+		self:ScheduleEvent("BigWigs_StartBar", 480, self, L["impalebarlocust"], 15, "Interface\\Icons\\INV_Weapon_ShortBlade_25")
+		self:ScheduleEvent("BigWigs_StartBar", 570, self, L["impalebarlocust"], 15, "Interface\\Icons\\INV_Weapon_ShortBlade_25")
 	end
 end
 
@@ -102,10 +126,17 @@ function BigWigsAnubrekhan:LocustCast( msg )
 	end
 end
 
+function BigWigsAnubrekhan:ImpaleCast( msg )
+	if string.find(msg, L["impaletrigger"]) then
+		self:TriggerEvent("BigWigs_SendSync", "AnubImpale")
+	end
+end
+
 function BigWigsAnubrekhan:BigWigs_RecvSync( sync )
 	if sync == "AnubLocustInc" then
 	        BigWigsThaddiusArrows:Direction("Run")
 		self:ScheduleEvent("bwanublocustinc", self.TriggerEvent, 3.25, self, "BigWigs_SendSync", "AnubLocustSwarm")
+		self:TriggerEvent("BigWigs_StopBar", self, string.format(L["impalebar"], 15))
 		if self.db.profile.locust then
 			self:TriggerEvent("BigWigs_Message", L["castwarn"], "Orange", true, "Alarm")
 			self:TriggerEvent("BigWigs_StartBar", self, L["castwarn"], 3, "Interface\\Icons\\Spell_Nature_InsectSwarm" )
@@ -116,8 +147,12 @@ function BigWigsAnubrekhan:BigWigs_RecvSync( sync )
 			self:ScheduleEvent("BigWigs_Message", 20, L["gainendwarn"], "Important")
 			self:TriggerEvent("BigWigs_StartBar", self, L["gainbar"], 20, "Interface\\Icons\\Spell_Nature_InsectSwarm")
 			self:TriggerEvent("BigWigs_Message", L["gainnextwarn"], "Urgent")
-			self:ScheduleEvent("BigWigs_Message", 75, L["gainwarn10sec"], "Urgent")
-			self:TriggerEvent("BigWigs_StartBar", self, L["gainincbar"], 85, "Interface\\Icons\\Spell_Nature_InsectSwarm")
+			self:ScheduleEvent("BigWigs_Message", 80, L["gainwarn10sec"], "Urgent")
+			self:TriggerEvent("BigWigs_StartBar", self, L["gainincbar"], 90, "Interface\\Icons\\Spell_Nature_InsectSwarm")
+		end
+	elseif sync == "AnubImpale" then
+		if self.db.profile.impale then
+			self:TriggerEvent("BigWigs_StartBar", self, L["impalebar"], 15, "Interface\\Icons\\INV_Weapon_ShortBlade_25")
 		end
 	end
 end
