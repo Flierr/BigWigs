ffed6bc132bd5d838c21b1b43e7ba09013bac94a
isitLoVe <isitl0v3@gmail.com>
2016-07-28 21:51:03 +0200

    Update Vaelastrasz.lua
    fixed burning adrenalin
    added flame breath bar

--- BigWigs/BWL/Vaelastrasz.lua
+++ BigWigs/BWL/Vaelastrasz.lua
@@ -1,4 +1,4 @@
-﻿------------------------------
+------------------------------
 --      Are you local?      --
 ------------------------------
 
@@ -14,12 +14,16 @@ local playerName = nil
 L:RegisterTranslations("enUS", function() return {
 	cmd = "Vaelastrasz",
 
-	trigger1 = "^([^%s]+) gains Burning",
-	trigger2 = "^You gain Burning",
-
+	trigger1 = "^([^%s]+) is afflicted by Burning Adrenaline",
+	trigger2 = "^You are afflicted by Burning Adrenaline",
+	trigger3 = "begins to cast Flame Breath",
+	
 	warn1 = "You are burning!",
 	warn2 = "%s is burning!",
-
+	
+	breath_bar = "Flame Breath",
+	breathcast_bar = "casting Flame Breath",
+	
 	youburning_cmd = "youburning",
 	youburning_name = "You are burning alert",
 	youburning_desc = "Warn when you are burning",
@@ -31,6 +35,10 @@ L:RegisterTranslations("enUS", function() return {
 	burningbar_cmd = "burningbar",
 	burningbar_name = "Burning Adrenaline bar",
 	burningbar_desc = "Shows a timer bar for Burning Adrenaline",
+	
+	breath_cmd = "breath",
+	breath_name = "Flame Breath bar",
+	breath_desc = "Shows a timer bar for Flame Breath",
 
 } end)
 
@@ -41,8 +49,8 @@ L:RegisterTranslations("enUS", function() return {
 BigWigsVaelastrasz = BigWigs:NewModule(boss)
 BigWigsVaelastrasz.zonename = AceLibrary("Babble-Zone-2.2")["Blackwing Lair"]
 BigWigsVaelastrasz.enabletrigger = boss
-BigWigsVaelastrasz.toggleoptions = {"youburning", "elseburning", "burningbar", "bosskill"}
-BigWigsVaelastrasz.revision = tonumber(string.sub("$Revision: 16639 $", 12, -3))
+BigWigsVaelastrasz.toggleoptions = {"youburning", "elseburning", "burningbar", "breath", "bosskill"}
+BigWigsVaelastrasz.revision = tonumber(string.sub("$Revision: 19009 $", 12, -3))
 
 ------------------------------
 --      Initialization      --
@@ -51,10 +59,18 @@ BigWigsVaelastrasz.revision = tonumber(string.sub("$Revision: 16639 $", 12, -3))
 function BigWigsVaelastrasz:OnEnable()
 	playerName = UnitName("player")
 
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS")
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_BUFFS")
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_BUFFS")
+	self:RegisterEvent("PLAYER_REGEN_ENABLED", "CheckForWipe")
+	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
+
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE")
+	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE")
+	
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
+	
+	self:RegisterEvent("BigWigs_RecvSync")
+	self:TriggerEvent("BigWigs_ThrottleSync", "VaelastraszBreath", 2)
 
 end
 
@@ -62,7 +78,20 @@ end
 --      Event Handlers      --
 ------------------------------
 
-function BigWigsVaelastrasz:CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS(msg)
+function BigWigsVaelastrasz:BigWigs_RecvSync(sync, rest)
+	if sync == self:GetEngageSync() and rest and rest == boss and not started then
+		started = true
+		if self:IsEventRegistered("PLAYER_REGEN_DISABLED") then
+			self:UnregisterEvent("PLAYER_REGEN_DISABLED")
+		end
+		self:TriggerEvent("BigWigs_StartBar", self, L["breath_bar"], 11, "Interface\\Icons\\Spell_Fire_SelfDestruct")
+	elseif sync == "VaelastraszBreath" and self.db.profile.breath then
+		self:TriggerEvent("BigWigs_StartBar", self, L["breathcast_bar"], 2, "Interface\\Icons\\Spell_Fire_Incinerate")
+	end
+end
+
+
+function BigWigsVaelastrasz:CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE(msg)
 	if string.find(msg, L["trigger2"]) and self.db.profile.youburning then
 			self:TriggerEvent("BigWigs_SetRaidIcon", playerName)
 		        self:TriggerEvent("BigWigs_Message", L["warn1"], "Personal", true, "Alarm")
@@ -73,7 +102,7 @@ function BigWigsVaelastrasz:CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS(msg)
 	end
 end
 
-function BigWigsVaelastrasz:CHAT_MSG_SPELL_PERIODIC_PARTY_BUFFS(msg)
+function BigWigsVaelastrasz:CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE(msg)
 	local _,_, n = string.find(msg, L["trigger1"])
 	if n then
 		if self.db.profile.elseburning then
@@ -87,7 +116,7 @@ function BigWigsVaelastrasz:CHAT_MSG_SPELL_PERIODIC_PARTY_BUFFS(msg)
 	end
 end
 
-function BigWigsVaelastrasz:CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_BUFFS(msg)
+function BigWigsVaelastrasz:CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE(msg)
 	local _,_, n = string.find(msg, L["trigger1"])
 	if n then
 		if self.db.profile.elseburning then
@@ -101,5 +130,8 @@ function BigWigsVaelastrasz:CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_BUFFS(msg)
 	end
 end
 
-
-
+function BigWigsVaelastrasz:CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE(msg)
+	if string.find(msg, L["trigger3"]) then
+		self:TriggerEvent("BigWigs_SendSync", "VaelastraszBreath")
+	end
+end
