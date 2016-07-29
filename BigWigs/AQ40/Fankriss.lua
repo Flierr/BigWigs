15b672a535a16206968f3c2856b3d8612dbee060
isitLoVe <isitl0v3@gmail.com>
2016-07-29 20:00:37 +0200

    Update Fankriss.lua
    entangle bar

--- BigWigs/AQ40/Fankriss.lua
+++ BigWigs/AQ40/Fankriss.lua
@@ -1,10 +1,13 @@
-﻿------------------------------
+------------------------------
 --      Are you local?      --
 ------------------------------
 
 local boss = AceLibrary("Babble-Boss-2.2")["Fankriss the Unyielding"]
 local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
 local worms
+local times = {}
+local prior = nil
+local started
 ----------------------------
 --      Localization      --
 ----------------------------
@@ -14,7 +17,18 @@ L:RegisterTranslations("enUS", function() return {
 	worm_cmd = "worm",
 	worm_name = "Worm Alert",
 	worm_desc = "Warn for Incoming Worms",
--- not working because no combatlog messages when a worm spawns on VG
+	
+	entangle_cmd = "entangle",
+	entangle_name = "Entangle Alert",
+	entangle_desc = "Warn for entangled players",
+	
+	entangletrigger = "(.*) (.*) afflicted by Entangle.",
+	entanglebar = "%s entangled",
+	
+	you = "You",
+	are = "are",
+	
+	-- not working because no combatlog messages when a worm spawns on VG
 	wormtrigger = "Fankriss the Unyielding casts Summon Worm.",
 	wormwarn = "Incoming Worm! (%d)",
 	wormbar = "Sandworm Enrage (%d)",
@@ -27,7 +41,7 @@ L:RegisterTranslations("enUS", function() return {
 BigWigsFankriss = BigWigs:NewModule(boss)
 BigWigsFankriss.zonename = AceLibrary("Babble-Zone-2.2")["Ahn'Qiraj"]
 BigWigsFankriss.enabletrigger = boss
-BigWigsFankriss.toggleoptions = {"worm", "bosskill"}
+BigWigsFankriss.toggleoptions = {"worm", "entangle", "bosskill"}
 BigWigsFankriss.revision = tonumber(string.sub("$Revision: 16639 $", 12, -3))
 
 ------------------------------
@@ -36,16 +50,39 @@ BigWigsFankriss.revision = tonumber(string.sub("$Revision: 16639 $", 12, -3))
 
 function BigWigsFankriss:OnEnable()
 	worms = 0
+	prior = nil
+	times = {}
+	started = nil
 	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_BUFF")
 	self:RegisterEvent("CHAT_MSG_COMBAT_HOSTILE_DEATH", "GenericBossDeath")
 
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "SprayEvent")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "SprayEvent")
+	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "SprayEvent")
+	
 	self:RegisterEvent("BigWigs_RecvSync")
 	self:TriggerEvent("BigWigs_ThrottleSync", "FankrissWormSpawn", .1)
+	self:TriggerEvent("BigWigs_ThrottleSync", "FankrissEntangle", 0)
 end
 
 ------------------------------
 --      Event Handlers      --
 ------------------------------
+
+function BigWigsFankriss:SprayEvent( msg )
+	if string.find(msg, L["entangletrigger"]) then
+		local _,_,wplayer,wtype = string.find(msg, L["entangletrigger"])
+		if wplayer and wtype then
+			if wplayer == L["you"] and wtype == L["are"] then
+				wplayer = UnitName("player")
+			end
+			local t = GetTime()
+			if ( not times[wplayer] ) or ( times[wplayer] and ( times[wplayer] + 10 ) < t) then
+				self:TriggerEvent("BigWigs_SendSync", "FankrissEntangle "..wplayer)
+			end
+		end
+	end
+end
 
 function BigWigsFankriss:CHAT_MSG_SPELL_CREATURE_VS_CREATURE_BUFF(msg)
 	if msg == L["wormtrigger"] then
@@ -54,7 +91,13 @@ function BigWigsFankriss:CHAT_MSG_SPELL_CREATURE_VS_CREATURE_BUFF(msg)
 end
 
 function BigWigsFankriss:BigWigs_RecvSync(sync, rest)
-	if sync ~= "FankrissWormSpawn" then return end
+	if sync == "FankrissEntangle" then
+		local t = GetTime()
+		if ( not times[rest] ) or ( times[rest] and ( times[rest] + 10 ) < t) then
+			if self.db.profile.entangle then self:TriggerEvent("BigWigs_StartBar", self, string.format(L["entanglebar"], rest), 7, "Interface\\Icons\\Spell_Nature_Web") end
+			times[rest] = t
+		end
+	elseif sync ~= "FankrissWormSpawn" then return end
 	if not rest then return end
 	rest = tonumber(rest)
 	if rest == (worms + 1) then
