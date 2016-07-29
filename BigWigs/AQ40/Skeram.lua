81cfb7192dde1b19cc20282c3c35213bb508e587
isitLoVe <isitl0v3@gmail.com>
2016-07-29 15:52:57 +0200

    Update Skeram.lua
    fixed split
    added arcane explosion cast

--- BigWigs/AQ40/Skeram.lua
+++ BigWigs/AQ40/Skeram.lua
@@ -1,4 +1,4 @@
-﻿------------------------------
+------------------------------
 --      Are you local?      --
 ------------------------------
 
@@ -13,6 +13,8 @@ L:RegisterTranslations("enUS", function() return {
 	aetrigger = "The Prophet Skeram begins to cast Arcane Explosion.",
 	mctrigger = "The Prophet Skeram begins to cast True Fulfillment.",
 	splittrigger = "The Prophet Skeram casts Summon Images.",
+	splittriggervg = "Prepare for the return of the ancient ones!",
+
 	aewarn = "Casting Arcane Explosion!",
 	mcwarn = "Casting Mind Control!",
 	mcplayer = "^([^%s]+) ([^%s]+) afflicted by True Fulfillment.$",
@@ -43,9 +45,9 @@ L:RegisterTranslations("enUS", function() return {
 	ae_name = "Arcane Explosion Alert",
 	ae_desc = "Warn for Arcane Explosion",
 	
---	split_cmd = "split",
---	split_name = "Split Alert",
---	split_desc = "Warn before Create Image",
+	split_cmd = "split",
+	split_name = "Split Alert",
+	split_desc = "Warn before Create Image",
 } end )
 
 ----------------------------------
@@ -55,8 +57,8 @@ L:RegisterTranslations("enUS", function() return {
 BigWigsSkeram = BigWigs:NewModule(boss)
 BigWigsSkeram.zonename = AceLibrary("Babble-Zone-2.2")["Ahn'Qiraj"]
 BigWigsSkeram.enabletrigger = boss
-BigWigsSkeram.toggleoptions = {"ae", "mc", "bosskill"}
-BigWigsSkeram.revision = tonumber(string.sub("$Revision: 16639 $", 12, -3))
+BigWigsSkeram.toggleoptions = {"ae", "mc", "split", "bosskill"}
+BigWigsSkeram.revision = tonumber(string.sub("$Revision: 19009 $", 12, -3))
 
 ------------------------------
 --      Initialization      --
@@ -90,16 +92,19 @@ end
 
 
 function BigWigsSkeram:CHAT_MSG_MONSTER_YELL(msg)
-    if string.find(arg1, L["win"]) then 
+    if string.find(msg, L["win"]) then 
         self:TriggerEvent("BigWigs_Message", string.format(AceLibrary("AceLocale-2.2"):new("BigWigs")["%s has been defeated"], self:ToString()), "Bosskill", nil, "Victory")
-	self.core:ToggleModuleActive(self, false)
-          end
+		self.core:ToggleModuleActive(self, false)
+	elseif string.find(msg, L["splittriggervg"]) and self.db.profile.split then
+		self:TriggerEvent("BigWigs_Message", L["splitwarn"], "Important")
+    end
 end
 
 
 function BigWigsSkeram:CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE(msg)
 	if msg == L["aetrigger"] and self.db.profile.ae then
 		self:TriggerEvent("BigWigs_Message", L["aewarn"], "Urgent")
+		self:TriggerEvent("BigWigs_StartBar", self, L["aewarn"], 1, "Interface\\Icons\\Spell_Nature_WispSplode")
 	elseif msg == L["mctrigger"] and self.db.profile.mc then
 		self:TriggerEvent("BigWigs_Message", L["mcwarn"], "Urgent")
 	elseif msg == L["splittrigger"] and self.db.profile.split then
